f175335185f9a39fa34a9259cc65c0ba640854a3
RMN0605 <20c1020027dr@edu.tech.ac.jp>
2023-12-21 02:04:48 +0900

    助けました

--- Assets/Member/Sakai/Scripts/Timer.cs
+++ Assets/Member/Sakai/Scripts/Timer.cs
@@ -15,6 +15,10 @@ public class Timer : MonoBehaviour
     public Image BlueSprite;
     [SerializeField]
     private AudioSource audioSource;
+    
+    private bool quarterPassed = false;
+    private bool halfPassed = false;
+    private bool threeQuartersPassed = false;
     private void Start()
     {
         GameManager.Instance.PauseEvent += PauseTimer;
@@ -28,24 +32,38 @@ public class Timer : MonoBehaviour
         while (true)
         {
             BlueSprite.fillAmount -= 1.0f / CountTime * Time.deltaTime;
-            if(BlueSprite.fillAmount <= 0.75 && BlueSprite.fillAmount > 0.5f)
+
+            // 4分の3が経過したとき
+            if (BlueSprite.fillAmount <= 0.75 && !quarterPassed)
             {
                 SoundManager.Instance.StartSE(SEtype.ChangeTimerPon, audioSource);
+                quarterPassed = true; // フラグを設定
             }
-            else if (BlueSprite.fillAmount <= 0.5 && BlueSprite.fillAmount > 0.25f)
+            // 半分が経過したとき
+            else if (BlueSprite.fillAmount <= 0.5 && !halfPassed)
             {
                 SoundManager.Instance.StartSE(SEtype.ChangeTimerPon, audioSource);
+                halfPassed = true; // フラグを設定
             }
-           else  if (BlueSprite.fillAmount <= 0.25)
+            // 4分の1が経過したとき
+            else if (BlueSprite.fillAmount <= 0.25 && !threeQuartersPassed)
             {
                 SoundManager.Instance.StartSE(SEtype.ChangeTimerPon, audioSource);
+                threeQuartersPassed = true; // フラグを設定
             }
-           else if (BlueSprite.fillAmount <= 0 || BlueSprite.fillAmount >= 1)
+            // タイマーが終了したとき
+            else if (BlueSprite.fillAmount <= 0)
             {
                 SoundManager.Instance.StartSE(SEtype.ChangeTimerTin, audioSource);
                 BlueSprite.fillAmount = 1.0f;
                 changeTimerEvent?.Invoke();
+
+                // フラグをリセット
+                quarterPassed = false;
+                halfPassed = false;
+                threeQuartersPassed = false;
             }
+
             yield return null;
         }
     }

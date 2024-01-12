e65cfa6fab87e6ec63805ff8f4144f7b6ed91f8d
shio-pkmn <gamershio@icloud.com>
2024-01-12 18:31:13 +0900

    ステージ選択画面の変更（未完成）

--- Assets/Member/yamashita/Scripts/StageSelection.cs
+++ Assets/Member/yamashita/Scripts/StageSelection.cs
@@ -9,6 +9,8 @@ public class StageSelection : MonoBehaviour
     [SerializeField] private GameObject _stage1;
     [SerializeField] private GameObject _stage2;
     [SerializeField] private GameObject _stage3;
+    [SerializeField] private GameObject _stage4;
+    [SerializeField] private GameObject _stage5;
 
     private float beforeInput;
 
@@ -21,7 +23,10 @@ public class StageSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //var a = _selectButton.transform.position;
+        
         float select = Input.GetAxis("Horizontal");
+        // 右のステージに移動
         if (select > 0f && beforeInput == 0f)
         {
             if (_selectButton.gameObject.transform.position == _stage1.transform.position)
@@ -29,16 +34,25 @@ public class StageSelection : MonoBehaviour
             else if (_selectButton.gameObject.transform.position == _stage2.transform.position)
                 _selectButton.gameObject.transform.position = _stage3.transform.position;
             else if (_selectButton.gameObject.transform.position == _stage3.transform.position)
+                _selectButton.gameObject.transform.position = _stage4.transform.position;
+            else if (_selectButton.gameObject.transform.position == _stage4.transform.position)
+                _selectButton.gameObject.transform.position = _stage5.transform.position;
+            else if (_selectButton.gameObject.transform.position == _stage5.transform.position)
                 _selectButton.gameObject.transform.position = _stage1.transform.position;
         }
+        // 左のステージに移動
         else if (select < 0f && beforeInput == 0f)
         {
             if (_selectButton.gameObject.transform.position == _stage1.transform.position)
-                _selectButton.gameObject.transform.position = _stage3.transform.position;
+                _selectButton.gameObject.transform.position = _stage5.transform.position;
             else if (_selectButton.gameObject.transform.position == _stage2.transform.position)
                 _selectButton.gameObject.transform.position = _stage1.transform.position;
             else if (_selectButton.gameObject.transform.position == _stage3.transform.position)
                 _selectButton.gameObject.transform.position = _stage2.transform.position;
+            else if (_selectButton.gameObject.transform.position == _stage4.transform.position)
+                _selectButton.gameObject.transform.position = _stage3.transform.position;
+            else if (_selectButton.gameObject.transform.position == _stage5.transform.position)
+                _selectButton.gameObject.transform.position = _stage4.transform.position;
         }
         beforeInput = select;
 
@@ -52,6 +66,14 @@ public class StageSelection : MonoBehaviour
             SceneManager.LoadScene("StageN");
         }
         if (_selectButton.gameObject.transform.position == _stage3.transform.position && Input.GetButton("Fire2"))
+        {
+            SceneManager.LoadScene("StageN");
+        }
+        if (_selectButton.gameObject.transform.position == _stage4.transform.position && Input.GetButton("Fire2"))
+        {
+            SceneManager.LoadScene("StageN");
+        }
+        if (_selectButton.gameObject.transform.position == _stage5.transform.position && Input.GetButton("Fire2"))
         {
             SceneManager.LoadScene("StageN");
         }

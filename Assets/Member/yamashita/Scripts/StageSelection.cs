fc92b7d0969125ebb4b61b7438a43fdb0a08622e
shio-pkmn <gamershio@icloud.com>
2024-01-19 11:41:00 +0900

    ステージ選択画面の変更

--- Assets/Member/yamashita/Scripts/StageSelection.cs
+++ Assets/Member/yamashita/Scripts/StageSelection.cs
@@ -13,69 +13,89 @@ public class StageSelection : MonoBehaviour
     [SerializeField] private GameObject _stage5;
 
     private float beforeInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    private int sizeX = 1920;
 
     // Update is called once per frame
     void Update()
     {
-        //var a = _selectButton.transform.position;
+        var buttonPos = _selectButton.gameObject.transform.position;
         
         float select = Input.GetAxis("Horizontal");
         // 右のステージに移動
         if (select > 0f && beforeInput == 0f)
         {
-            if (_selectButton.gameObject.transform.position == _stage1.transform.position)
-                _selectButton.gameObject.transform.position = _stage2.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage2.transform.position)
-                _selectButton.gameObject.transform.position = _stage3.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage3.transform.position)
-                _selectButton.gameObject.transform.position = _stage4.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage4.transform.position)
-                _selectButton.gameObject.transform.position = _stage5.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage5.transform.position)
-                _selectButton.gameObject.transform.position = _stage1.transform.position;
+            if (buttonPos == _stage1.transform.position)
+            {
+                StageSelect(1);
+            }
+            else if (buttonPos == _stage2.transform.position)
+            {
+                StageSelect(2);
+            }
+            else if (buttonPos == _stage3.transform.position)
+            {
+                StageSelect(3);
+            }
+            else if (buttonPos == _stage4.transform.position)
+            {
+                StageSelect(4);
+            }
+            else if (buttonPos == _stage5.transform.position)
+            {
+                StageSelect(0);
+            }
         }
         // 左のステージに移動
         else if (select < 0f && beforeInput == 0f)
         {
-            if (_selectButton.gameObject.transform.position == _stage1.transform.position)
-                _selectButton.gameObject.transform.position = _stage5.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage2.transform.position)
-                _selectButton.gameObject.transform.position = _stage1.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage3.transform.position)
-                _selectButton.gameObject.transform.position = _stage2.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage4.transform.position)
-                _selectButton.gameObject.transform.position = _stage3.transform.position;
-            else if (_selectButton.gameObject.transform.position == _stage5.transform.position)
-                _selectButton.gameObject.transform.position = _stage4.transform.position;
+            if (buttonPos == _stage1.transform.position)
+            {
+                StageSelect(4);
+            }
+            else if (buttonPos == _stage2.transform.position)
+            {
+                StageSelect(0);
+            }
+            else if (buttonPos == _stage3.transform.position)
+            {
+                StageSelect(1);
+            }
+            else if (buttonPos == _stage4.transform.position)
+            {
+                StageSelect(2);
+            }
+            else if (buttonPos == _stage5.transform.position)
+            {
+                StageSelect(3);
+            }
         }
         beforeInput = select;
 
-        if (_selectButton.gameObject.transform.position == _stage1.transform.position && Input.GetButton("Fire2"))
+        if (buttonPos == _stage1.transform.position && Input.GetButton("Fire2"))
         {
             // 仮のシーン
             SceneManager.LoadScene("StageN");
         }
-        if (_selectButton.gameObject.transform.position == _stage2.transform.position && Input.GetButton("Fire2"))
+        if (buttonPos == _stage2.transform.position && Input.GetButton("Fire2"))
         {
             SceneManager.LoadScene("StageN");
         }
-        if (_selectButton.gameObject.transform.position == _stage3.transform.position && Input.GetButton("Fire2"))
+        if (buttonPos == _stage3.transform.position && Input.GetButton("Fire2"))
         {
             SceneManager.LoadScene("StageN");
         }
-        if (_selectButton.gameObject.transform.position == _stage4.transform.position && Input.GetButton("Fire2"))
+        if (buttonPos == _stage4.transform.position && Input.GetButton("Fire2"))
         {
             SceneManager.LoadScene("StageN");
         }
-        if (_selectButton.gameObject.transform.position == _stage5.transform.position && Input.GetButton("Fire2"))
+        if (buttonPos == _stage5.transform.position && Input.GetButton("Fire2"))
         {
             SceneManager.LoadScene("StageN");
         }
+    }
+
+    private void StageSelect(int num)
+    {
+        transform.localPosition = new Vector2(-sizeX * num, 0);
     }
 }
